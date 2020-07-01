bf7841774a02c178e27da89ad1035fa1fed94404
Karyakina Tatiana <tinka127@mail.ru>
2020-07-01 18:44:44 +0500

    fix

--- js/server-load.js
+++ js/server-load.js
@@ -1,13 +1,20 @@
 'use strict';
 
 (function () {
-  window.load = function (onSuccess) {
+  window.load = function (onSuccess, onError) {
+    var statusCode = {
+      OK: 200
+    };
     var URL = 'https://javascript.pages.academy/keksobooking/data';
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      onSuccess(xhr.response);
+      if (xhr.status === statusCode.OK) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
     });
 
     xhr.open('GET', URL);
