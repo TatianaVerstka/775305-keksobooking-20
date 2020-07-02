b4ed1902df582ef1599fd281e655af3f640e4615
Karyakina Tatiana <tinka127@mail.ru>
2020-07-02 21:29:06 +0500

    fix

--- js/pin.js
+++ js/pin.js
@@ -21,6 +21,18 @@
     mapPinsElement.appendChild(docFragment);
   }
 
+  function onError(errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  }
+
   window.removeMapPins = function () {
     var elsPins = document.querySelectorAll('.map__pin');
     elsPins.forEach(function (elPin) {
@@ -31,7 +43,7 @@
   };
 
   window.renderPins = function () {
-    window.load(onSuccess);
+    window.load(onSuccess, onError);
   };
 
 })();
