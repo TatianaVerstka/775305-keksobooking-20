c36b1d761ed11e74aacc939ba9a5a3b27b4aefbe
Karyakina Tatiana <tinka127@mail.ru>
2020-07-08 01:12:53 +0500

    fix

--- js/render-popup.js
+++ js/render-popup.js
@@ -3,13 +3,26 @@
 (function () {
   function renderPopup(el) {
     var popupElement = window.cloneElements('#card', '.popup');
+    var offerType = el.offer.type;
+    var newType = ''
+
+    if (offerType === 'flat') {
+      newType = 'Квaртира';
+    } else if (offerType === 'bungalo') {
+      newType = 'Бунгало';
+    } else if (offerType === 'house') {
+      newType = 'Дом';
+    } else {
+      newType = 'Дворец';
+    }
     popupElement.querySelector('.popup__title').textContent = el.offer.title;
     popupElement.querySelector('.popup__text--address').textContent = el.offer.address;
     popupElement.querySelector('.popup__text--price').textContent = el.offer.price + '₽/ночь';
-    popupElement.querySelector('.popup__type').textContent = el.offer.type;
+    popupElement.querySelector('.popup__type').textContent = newType;
     popupElement.querySelector('.popup__text--capacity').textContent = el.offer.rooms + ' комнаты для ' + el.offer.guests + ' гостей';
     popupElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + el.offer.checkin + ', выезд до ' + el.offer.checkout;
     popupElement.querySelector('.popup__features').textContent = el.offer.features;
+    popupElement.querySelector('.popup__features').style = 'word-break: break-all';
     popupElement.querySelector('.popup__description').textContent = el.offer.description;
     popupElement.querySelector('.popup__photos').src = el.offer.photos;
     popupElement.querySelector('.popup__avatar').src = el.author.avatar;
